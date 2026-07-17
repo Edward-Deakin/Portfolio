ce4af484b2155788511f3ea515cdfd5f80b6a81e
Edward-Deakin <deakinedward53@outlook.com>
2026-07-17 22:28:49 +0100

    feat: add crawler files (robots, sitemap etc.) feat: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,13 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://edwarddeakin.uk',
+
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
