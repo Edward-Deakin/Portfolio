b6f3301879065de6ef3022dcfc5b6ff051480b2e
Edward-Deakin <deakinedward53@outlook.com>
2026-07-17 22:05:25 +0100

    fix: track ProjectCarousel.tsx to git

---
+++ src/components/ProjectCarousel.tsx
@@ -0,0 +1,30 @@
+// src/components/ProjectCarousel.tsx
+import ProjectCard from './ProjectCard';
+import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from './ui/carousel';
+
+type Project = {
+    title: string;
+    description: string;
+    link: string;
+    tags?: string[];
+};
+
+type Props = {
+    projects: Project[];
+};
+
+export default function ProjectCarousel({ projects }: Props) {
+    return (
+        <Carousel opts={{ align: 'start' }} className="w-full">
+            <CarouselContent>
+                {projects.map((project) => (
+                    <CarouselItem key={project.title} className="basis-full sm:basis-1/2 lg:basis-1/3">
+                        <ProjectCard {...project} />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    );
+}
